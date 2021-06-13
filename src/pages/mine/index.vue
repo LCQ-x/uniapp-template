e5b01acb43b904cbd21b7920a387e81aa7724b30
LCQQQQ <2286552044@qq.com>
2021-06-13 21:04:38 +0800

    加入Eslint配置

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -1,23 +1,34 @@
 <template>
-  <view class=""></view>
+  <view class="mine-container">
+    <view class="mine-header">
+      <view>1</view>
+    </view>
+    <view class="mine-order" />
+    <view class="navigate-cell-container" />
+  </view>
 </template>
 
 <script>
 export default {
-  data () {
-
-    return {
-
-    };
+  data() {
+    return {}
   },
-  onLoad () {
-
-  },
-  methods: {
-
-  }
+  onLoad() { },
+  methods: {}
 }
 </script>
 
-<style lang='scss' scoped>
+<style lang="scss" scoped>
+page {
+  background: url("../../static/mine/bg-mine.png");
+  background-repeat: no-repeat;
+  background-size: 100% auto;
+}
+
+.mine-header {
+  margin: 50rpx 0 0 0;
+  padding: 0 20rpx;
+  height: 200rpx;
+  background-color: aquamarine;
+}
 </style>
